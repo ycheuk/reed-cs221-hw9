e172fc4939a236714f2de864ad10b0d9e516f966
Yik Yin Cheuk <101148925+ycheuk@users.noreply.github.com>
2022-04-11 16:13:52 -0700

    Update tsp.cc

--- tsp.cc
+++ tsp.cc
@@ -9,3 +9,18 @@ int main(int argc, char** argv) {
     std::cerr << "No File Provided\n";
       return 1;
   }
+  Cities map;
+  readStream >> map;    // Reads in the cities from the given file
+
+  int max = 1000000; // Iterates over a fixed number
+
+  // Generates a new random permutation
+  for(int i = 0; i < max; i++) {
+    order = map.random_permutation(number_of_cities);
+    city = map.reorder(order);
+    // Evaluates distance to travel the cities along this ordering;
+    double newLen = map.total_path_distance(order);
+    if(newLen < shortestLen) {
+        shortestLen = newLen;
+        std::cout << i << "\t" << shortestLen << "\n";
+    }
