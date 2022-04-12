d5be35571ded74627bef46367d48d2f952967314
Yik Yin Cheuk <101148925+ycheuk@users.noreply.github.com>
2022-04-12 15:45:48 -0700

    Update cities.cc

--- cities.cc
+++ cities.cc
@@ -2,11 +2,28 @@
 // Coded by Yik Yin Cheuk and Vaughn Zaayer
 
 #include "cities.hh"
-#include <vector> 
+#include <vector>
 
-// Returns the distance between two city coord pairs
-double Cities::dist_between(cityA, cityB) const {
-  // Cast as doubles so hypot returns doubles
+std::istream& operator >> (std::istream& in, Cities& cities_ls){
+    int x;
+    while(input >> x) { // While stream is not done
+        Cities::coord_t pair;
+        pair.first = x;
+        input >> pair.second;
+        cities_ls.city_elements_.push_back(pair);
+    }
+    return input;
+}
+
+std::ostream& operator << (std::ostream& os, Cities& cities_ls){
+    for(Cities::coord_t elm: cities_ls.city_elements_){
+        output << elm.first << "\t" << elm.second << "\n";
+    }
+    return output;
+}
+
+// Used to calculate the distance between two city's coordinates
+double Cities::dist_between(coord_t cityA, coord_t cityB) const {
   return std::hypot(static_cast<double>(cityA.first-cityB.first), static_cast<double>(cityA.second-cityB.second));
 }
 
@@ -16,16 +33,14 @@ double Cities::total_path_distance(const permutation_t& ordering) const {
   // The total distance to traverse the cities in the order given by the permutation
   double distance = dist_between(cityPath.at(0), cityPath.at(cityPath.size()-1));
   // Loop the distance between cities
-  for (unsigned int i = 1; i < cityPath.size(); i++)
-  {
+  for (unsigned int i = 1; i < cityPath.size(); i++){
       distance += totalDistance(cityPath.at(i), cityPath.at(i - 1));
   }
   return distance;
 }
 
 Cities Cities::reorder(const permutation_t& ordering) const {
-  auto permuted_cities = new Cities();
-  std::vector<coord_t> newOrder = {} // Empty vector
+  std::vector<coord_t> newOrder;
   for (int i : ordering){
     newOrder.push_back(cityVect_[i]);
   }
@@ -41,3 +56,8 @@ Cities::random_permutation(unsigned len) const{
       permuted_nums.push_back(i);
   }
 }
+
+// Creates vector of coordinates
+Cities::Cities(std::vector<coord_t> cities)
+  :cityVect_(cities)
+{}
