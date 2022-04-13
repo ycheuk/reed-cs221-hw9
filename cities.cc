c2eac85321263ee44609efbc4cff261e05370e56
Yik Yin Cheuk <101148925+ycheuk@users.noreply.github.com>
2022-04-13 00:05:10 -0700

    Update cities.cc

--- cities.cc
+++ cities.cc
@@ -9,19 +9,19 @@ Cities::Cities(std::vector<coord_t> cities)
   :cityVect_(cities)
 {}
 
-std::istream& operator >> (std::istream& in, Cities& cities_ls){
+std::istream& operator >> (std::istream& input, Cities& cities){
     int x;
     while(input >> x) { // While stream is not done
         Cities::coord_t pair;
         pair.first = x;
         input >> pair.second;
-        cities_ls.city_elements_.push_back(pair);
+        cities.cityVect_.push_back(pair);
     }
     return input;
 }
 
-std::ostream& operator << (std::ostream& os, Cities& cities_ls){
-    for(Cities::coord_t elm: cities_ls.city_elements_){
+std::ostream& operator << (std::ostream& output, Cities& cities){
+    for(Cities::coord_t elm: cities.cityVect_){
         output << elm.first << "\t" << elm.second << "\n";
     }
     return output;
@@ -29,17 +29,18 @@ std::ostream& operator << (std::ostream& os, Cities& cities_ls){
 
 // Used to calculate the distance between two city's coordinates
 double Cities::dist_between(coord_t cityA, coord_t cityB) const {
+  // Thank you Gabe for telling me there was a better method :)
   return std::hypot(static_cast<double>(cityA.first-cityB.first), static_cast<double>(cityA.second-cityB.second));
 }
 
 // Returns total path distance
 double Cities::total_path_distance(const permutation_t& ordering) const {
-  auto cityPath = reorder(ordering).cityElements;
+  auto cityPath = reorder(ordering).cityVect_;
   // The total distance to traverse the cities in the order given by the permutation
   double distance = dist_between(cityPath.at(0), cityPath.at(cityPath.size()-1));
   // Loop the distance between cities
   for (unsigned int i = 1; i < cityPath.size(); i++){
-      distance += totalDistance(cityPath.at(i), cityPath.at(i - 1));
+      distance += dist_between(cityPath.at(i), cityPath.at(i - 1));
   }
   return distance;
 }
